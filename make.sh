7c9b7bb73d448f9946005bce68c4dee8d585d740
disenone <disenonec@gmail.com>
2016-12-27 19:03:09 +0800

    edit build

--- make.sh
+++ make.sh
@@ -1,1 +1,2 @@
-gyp --depth=. --generator-output=build -f make kcpuv-plus.gyp
+git submodule update --init --recursive
+./third-party/gyp/gyp --depth=. --generator-output=build -f make kcpuv-plus.gyp
