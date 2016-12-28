7dbb134a0b6331e174f24ee3b671cc85cf7b310c
disenone <disenonec@gmail.com>
2016-12-28 14:13:27 +0800

    edit build on linux

--- make.sh
+++ make.sh
@@ -1,1 +1,6 @@
-./third-party/gyp/gyp --depth=. --generator-output=build --build=Release --build=Debug -f make kcpuv-plus.gyp
+#!/bin/sh
+./third-party/gyp/gyp --depth=. --generator-output=build -f make kcpuv-plus.gyp
+cd build
+make BUILDTYPE=Release
+make BUILDTYPE=Debug
+
