095ab9f3403129814593c26a4d2b630e3c69fd29
disenone <disenonec@gmail.com>
2017-01-03 19:22:00 +0800

    edit platform macro

---
+++ src/kcpuvp.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <platform.h>
+
+#ifdef PLATFORM_WINDOWS
+#   ifdef KCPUVP_DLL
+#       define KCPUVP_API __declspec(dllexport)
+#   elif defined(KCPUVP_LIB)
+#       define KCPUVP_API __attribute ((visibility("default")))
+#   else
+#       define KCPUVP_API __declspec(dllimport)
+#   endif
+#else
+#define KCPUVP_API
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+int kcpuv_send_data();
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
