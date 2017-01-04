68bde2fb9e96769305141cb86833729b1906115d
disenone <disenonec@gmail.com>
2017-01-05 00:13:08 +0800

    build underground

--- build_all.bat
+++ build_all.bat
@@ -3,8 +3,8 @@ git submodule update --init --recursive
 ::build libuv
 cd third-party\libuv
 git checkout v1.10.1
-.\..\gyp\gyp.bat -I common.gypi --depth=. --generator-output=out --build=Release --build=Debug -Dhost_arch=x64 -Dtarget_arch=x64 -Duv_library=static_library -f msvs uv.gyp
-.\..\gyp\gyp.bat -I common.gypi --depth=. --generator-output=out --build=Release --build=Debug -Dhost_arch=x64 -Dtarget_arch=x64 -Duv_library=shared_library -f msvs uv.gyp
+start /b .\..\gyp\gyp.bat -I common.gypi --depth=. --generator-output=out --build=Release --build=Debug -Dhost_arch=x64 -Dtarget_arch=x64 -Duv_library=static_library -f msvs uv.gyp
+start /b .\..\gyp\gyp.bat -I common.gypi --depth=. --generator-output=out --build=Release --build=Debug -Dhost_arch=x64 -Dtarget_arch=x64 -Duv_library=shared_library -f msvs uv.gyp
 cd ..\..
 
 .\make.bat
