eee7d726393de17e9d66723190ae9d0bd470443a
disenone <disenonec@gmail.com>
2017-01-04 17:27:01 +0800

    add libuv

--- build_all.bat
+++ build_all.bat
@@ -1,2 +1,5 @@
 git submodule update --init --recursive
+cd third_party\libuv
+git checkout v1.10.1
+cd ..\..
 .\make.bat
--- build_all.sh
+++ build_all.sh
@@ -1,2 +1,18 @@
 git submodule update --init --recursive
+mkdir -p third-party/libuv/build/ && cp -r third-party/gyp third-party/libuv/build/
+
+# build libuv
+cd third-party/libuv
+git checkout v1.10.1
+./gyp_uv.py -f make
+make -C out
+BUILDTYPE=Release make -C out
+./gyp_uv.py -f make -Duv_library=shared_library
+make -C out
+BUILDTYPE=Release make -C out
+cp -f out/Debug/lib.target/libuv.so.1 out/Debug/libuv.so
+cp -f out/Release/lib.target/libuv.so.1 out/Release/libuv.so
+cd ../..
+
 ./make.sh
+
