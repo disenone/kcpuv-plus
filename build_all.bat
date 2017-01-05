9e8bb8bb460756ede59da8fcaa531c81660dfb8e
disenone <disenonec@gmail.com>
2017-01-05 13:06:18 +0800

    build on windows

--- build_all.bat
+++ build_all.bat
@@ -4,7 +4,8 @@ git submodule update --init --recursive
 cd third-party\libuv
 git checkout v1.10.1
 start /b .\..\gyp\gyp.bat -I common.gypi --depth=. --generator-output=out --build=Release --build=Debug -Dhost_arch=x64 -Dtarget_arch=x64 -Duv_library=static_library -f msvs uv.gyp
+sleep 60
 start /b .\..\gyp\gyp.bat -I common.gypi --depth=. --generator-output=out --build=Release --build=Debug -Dhost_arch=x64 -Dtarget_arch=x64 -Duv_library=shared_library -f msvs uv.gyp
 cd ..\..
-
+sleep 60
 .\make.bat
